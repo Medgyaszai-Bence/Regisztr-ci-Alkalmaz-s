6d73d58a982a78ce3d6da2974ff847d60cfabfe6
Petrik Lajos Szg <info@petrik.hu>
2018-10-19 16:34:31 +0200

    regisztráció

---
+++ RegisztracioAlkalmazas/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace RegisztracioAlkalmazas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNev = new System.Windows.Forms.TextBox();
+            this.textBoxDatum = new System.Windows.Forms.TextBox();
+            this.textBoxHobbi = new System.Windows.Forms.TextBox();
+            this.buttonHozzaad = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.contentBox = new System.Windows.Forms.ListBox();
+            this.radioButtonF = new System.Windows.Forms.RadioButton();
+            this.radioButtonN = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBoxNev
+            // 
+            this.textBoxNev.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxNev.Location = new System.Drawing.Point(136, 18);
+            this.textBoxNev.Name = "textBoxNev";
+            this.textBoxNev.Size = new System.Drawing.Size(100, 24);
+            this.textBoxNev.TabIndex = 0;
+            // 
+            // textBoxDatum
+            // 
+            this.textBoxDatum.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxDatum.Location = new System.Drawing.Point(136, 56);
+            this.textBoxDatum.Name = "textBoxDatum";
+            this.textBoxDatum.Size = new System.Drawing.Size(100, 24);
+            this.textBoxDatum.TabIndex = 1;
+            // 
+            // textBoxHobbi
+            // 
+            this.textBoxHobbi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxHobbi.Location = new System.Drawing.Point(280, 140);
+            this.textBoxHobbi.Name = "textBoxHobbi";
+            this.textBoxHobbi.Size = new System.Drawing.Size(100, 24);
+            this.textBoxHobbi.TabIndex = 2;
+            // 
+            // buttonHozzaad
+            // 
+            this.buttonHozzaad.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonHozzaad.Location = new System.Drawing.Point(294, 170);
+            this.buttonHozzaad.Name = "buttonHozzaad";
+            this.buttonHozzaad.Size = new System.Drawing.Size(86, 23);
+            this.buttonHozzaad.TabIndex = 3;
+            this.buttonHozzaad.Text = "Hozzáad";
+            this.buttonHozzaad.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonSave.Location = new System.Drawing.Point(224, 267);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Mentés";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonOpen.Location = new System.Drawing.Point(305, 267);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 5;
+            this.buttonOpen.Text = "Betöltés";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            // 
+            // contentBox
+            // 
+            this.contentBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.contentBox.FormattingEnabled = true;
+            this.contentBox.ItemHeight = 18;
+            this.contentBox.Items.AddRange(new object[] {
+            "Úszás",
+            "Horgászat",
+            "Futás"});
+            this.contentBox.Location = new System.Drawing.Point(280, 39);
+            this.contentBox.Name = "contentBox";
+            this.contentBox.Size = new System.Drawing.Size(100, 94);
+            this.contentBox.TabIndex = 6;
+            // 
+            // radioButtonF
+            // 
+            this.radioButtonF.AutoSize = true;
+            this.radioButtonF.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioButtonF.Location = new System.Drawing.Point(136, 98);
+            this.radioButtonF.Name = "radioButtonF";
+            this.radioButtonF.Size = new System.Drawing.Size(35, 22);
+            this.radioButtonF.TabIndex = 7;
+            this.radioButtonF.TabStop = true;
+            this.radioButtonF.Text = "F";
+            this.radioButtonF.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonN
+            // 
+            this.radioButtonN.AutoSize = true;
+            this.radioButtonN.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioButtonN.Location = new System.Drawing.Point(199, 98);
+            this.radioButtonN.Name = "radioButtonN";
+            this.radioButtonN.Size = new System.Drawing.Size(37, 22);
+            this.radioButtonN.TabIndex = 8;
+            this.radioButtonN.TabStop = true;
+            this.radioButtonN.Text = "N";
+            this.radioButtonN.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(16, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Név:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(16, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Szül. dátum:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(16, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Nem:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(257, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 18);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Kedvenc hobbi:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(208, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 18);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Új hobbi:";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Szöveges fájlok|*.txt|Minden fájl|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 302);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButtonN);
+            this.Controls.Add(this.radioButtonF);
+            this.Controls.Add(this.contentBox);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonHozzaad);
+            this.Controls.Add(this.textBoxHobbi);
+            this.Controls.Add(this.textBoxDatum);
+            this.Controls.Add(this.textBoxNev);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Regisztráció";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNev;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonN;
+        private System.Windows.Forms.RadioButton radioButtonF;
+        private System.Windows.Forms.ListBox contentBox;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonHozzaad;
+        private System.Windows.Forms.TextBox textBoxHobbi;
+        private System.Windows.Forms.TextBox textBoxDatum;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ RegisztracioAlkalmazas/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegisztracioAlkalmazas
+{
+    public partial class Form1 : Form
+    {
+
+       static string[] tomb = new string[5];
+
+        public Form1()
+        {
+            InitializeComponent();
+         
+            buttonSave.Click += (sender, e) =>
+            {
+                Mentes();
+            };
+
+            buttonOpen.Click += (sender, e) =>
+            {
+                Megnyitas();
+            };
+
+        }
+
+
+
+        private void Mentes()
+        {
+
+           
+            string tartalom1 = contentBox.Items.ToString();
+            string tartalomNev = textBoxNev.Text;
+            string tartalomDatum = textBoxDatum.Text;
+            string tartalomNem = radioNem();
+        
+            tomb[0] = tartalom1.ToString();
+            tomb[1] = tartalomNev.ToString();
+            tomb[2] = tartalomDatum.ToString();
+            tomb[3] = tartalomNem.ToString();
+            tomb[4] = 
+
+            saveFileDialog.FileName = "";
+            var eredmeny = saveFileDialog.ShowDialog(this);
+            if (eredmeny == DialogResult.OK)
+            {
+                string fileNev = saveFileDialog.FileName;
+               
+                File.WriteAllLines(fileNev, tomb);
+            }
+
+        }
+
+
+        private void Megnyitas()
+        {
+            saveFileDialog.FileName = "";
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                contentBox.Text = File.ReadAllText(saveFileDialog.FileName);
+            }
+        }
+
+        private string radioNem()
+        {
+            if(radioButtonF.Checked == true)
+            {
+                return "Férfi";
+            }else if(radioButtonN.Checked == true)
+            {
+                return "Nő";
+            }
+            else
+            {
+                return "Nem adott meg nemet";
+            }
+        }
+
+
+        private void Hozzaad()
+        {
+            string hozzaad = textBoxHobbi.Text;
+            
+
+        }
+
+    }
+}
