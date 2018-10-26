529ea5c1466296a7e23cf001bd14a35b7e159640
Petrik Lajos Szg <info@petrik.hu>
2018-10-26 16:21:44 +0200

    félig működő

--- RegisztracioAlkalmazas/Form1.cs
+++ RegisztracioAlkalmazas/Form1.cs
@@ -14,7 +14,7 @@ namespace RegisztracioAlkalmazas
     public partial class Form1 : Form
     {
 
-       static string[] tomb = new string[5];
+       static string[] tomb = new string[100];
 
         public Form1()
         {
@@ -30,24 +30,42 @@ namespace RegisztracioAlkalmazas
                 Megnyitas();
             };
 
+            buttonHozzaad.Click += (sender, e) =>
+            {
+                Hozzaad();
+            };
+
         }
 
 
 
         private void Mentes()
         {
-
            
-            string tartalom1 = contentBox.Items.ToString();
+            string[] contentBoxTomb = new string[contentBox.Items.Count];
+           
+           
+
+
+            string tartalom1 = "Kedvenc: " + contentBox.SelectedItem.ToString();
             string tartalomNev = textBoxNev.Text;
             string tartalomDatum = textBoxDatum.Text;
             string tartalomNem = radioNem();
-        
-            tomb[0] = tartalom1.ToString();
-            tomb[1] = tartalomNev.ToString();
-            tomb[2] = tartalomDatum.ToString();
-            tomb[3] = tartalomNem.ToString();
-            tomb[4] = 
+           
+            tomb[0] = tartalomNev.ToString();
+            tomb[1] = tartalomDatum.ToString();
+            tomb[2] = tartalomNem.ToString();
+            tomb[3] = tartalom1;
+
+
+            for (int i = 0; i < contentBoxTomb.Length; i++)
+            {
+
+                contentBoxTomb[i] = contentBox.Items[i].ToString();
+                tomb[i+4] = contentBoxTomb[i];
+
+            }
+
 
             saveFileDialog.FileName = "";
             var eredmeny = saveFileDialog.ShowDialog(this);
@@ -56,17 +74,41 @@ namespace RegisztracioAlkalmazas
                 string fileNev = saveFileDialog.FileName;
                
                 File.WriteAllLines(fileNev, tomb);
+                System.Windows.Forms.Application.Exit();
             }
 
         }
 
 
         private void Megnyitas()
-        {
+        {          
+            string[] beolvasottElemek = new string[tomb.Length];
             saveFileDialog.FileName = "";
             if (openFileDialog.ShowDialog(this) == DialogResult.OK)
             {
-                contentBox.Text = File.ReadAllText(saveFileDialog.FileName);
+
+                //contentBox.Text = File.ReadAllText(saveFileDialog.FileName);
+                beolvasottElemek = File.ReadAllLines(openFileDialog.FileName);
+
+                textBoxNev.Text = beolvasottElemek[0];
+                textBoxDatum.Text = beolvasottElemek[1];
+                if (beolvasottElemek[2] == "Férfi")
+                {
+                    radioButtonF.Checked = true;
+                }
+                else if(beolvasottElemek[2] == "Nő")
+                {
+                    radioButtonN.Checked = true;
+                }
+                //contentBox.Items = beolvasottElemek[3];
+
+              /*  for (int i = 0; i < contentBox.Items.Count; i++)
+                {
+                    contentBox.Items.Add(beolvasottElemek[i + 4]);
+                }
+                */
+                
+                            
             }
         }
 
@@ -89,9 +131,12 @@ namespace RegisztracioAlkalmazas
         private void Hozzaad()
         {
             string hozzaad = textBoxHobbi.Text;
+            contentBox.Items.Add(hozzaad);
+            textBoxHobbi.Text = "";
             
 
         }
 
+       
     }
 }
